200428f04ba1c787cf44c59ce32e249593e0a785
kushagra <kushagra.bhagurkar21@gmail.com>
2021-09-26 17:31:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import react, { Component } from "react";
+import { interval } from "rxjs";
+import { startWith, switchMap } from "rxjs/operators";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    profiles: [],
+    isLoading: false,
+  };
+  /*async fetchData() {
+    this.setState({ isLoading: true });
+    const res = await fetch("http://localhost:3000/profiles");
+    const data = await res.json();
+    this.setState({ profiles: data, isLoading: false });
+  }
+  async componentDidMount() {
+    await this.fetchData();
+    this.interval = setInterval(() => this.fetchData(), 1000);
+  }
+  componentWillUnmount(){
+    clearInterval(interval)
+  }*/
+  async componentDidMount() {
+    this.setState({ isLoading: true });
+    const request = interval(1000).pipe(
+      startWith(0),
+      switchMap(async () =>
+        fetch("http://localhost:3000/profiles").then((res) => res.json())
+      )
+    );
+    request.subscribe((data) => {
+      this.setState({ profiles: data, isLoading: false });
+    });
+  }
+  render() {
+    const { profiles, isLoading } = this.state;
+    if (isLoading) {
+      return <p>Loading..</p>;
+    }
+    return (
+      <div>
+        {profiles.map((data) => {
+          return <p>{data.email}</p>;
+        })}
+      </div>
+    );
+  }
 }
-
 export default App;
